47a7d2f9e7b014a278bb26c3c4ce0a13c6bbaa28
Kelvin de Reus <31496522+kelvin-codes-stuff@users.noreply.github.com>
2022-12-08 11:56:38 +0100

    Added class

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 from datetime import datetime
 
-# Cache json storage
-cache_files = {}
+
 
 class cache():
+    # Cache json storage
+    files = {}
+
     def write(key, data, expire_time):
         now = datetime.now()
-        cache_files[key] = {
+
+        cache.files[key] = {
             "data": data,
             "expire": int(now.strftime("%H%M%S")) + expire_time,
         }
-        return cache_files[data]["data"]
+        return cache.files[key]["data"]
 
 
     def read(key):
-        if not (cache_files.get(key) is None):
-            return cache_files[key]["data"]
+        if not (cache.files.get(key) is None):
+            return cache.files[key]["data"]
         return None
 
 
     def check(key):
         now = datetime.now()
 
-        cache_file_expire = cache_files[key]["expire"]
+        cache_file_expire = cache.files[key]["expire"]
         if int(now.strftime("%H%M%S")) > cache_file_expire:
             return None
         else:
-            return "IDK"
+            return "OK"
